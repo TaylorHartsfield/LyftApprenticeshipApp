ff7fc9ab5384032896a1853002b72e98825d655c
Taylor Hartsfield <taylormhartsfield@gmail.com>
2022-05-09 21:31:33 -0600

    POST request complete

--- app.py
+++ app.py
@@ -2,22 +2,17 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET'])
-def welcome():
-    return "Welcome to this request"
-
 @app.route('/test', methods=['POST'])
 def string_to_cut():
     string = request.json
     if string:
-        cut_srting = string_cutter(string['string_to_cut'])
+        cut_string = string_cutter(string['string_to_cut'])
         return jsonify({"return_string": cut_string})
     return jsonify({"error": "Invalid Entry"})
 
-
-
-
 def string_cutter(string):
+    if len(string)<3:
+        return "String value too short. Must enter a string of at least 3 characters."
     cut_string = [string[i] for i in range(len(string)) if (i+1)%3==0]
     return ''.join(cut_string)
 
