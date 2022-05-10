b632df904d939f63c0843df4bdc4129097877b71
Taylor Hartsfield <taylormhartsfield@gmail.com>
2022-05-09 20:34:57 -0600

    updated more desciptive names in string_cutter function

--- app.py
+++ app.py
@@ -1,15 +1,15 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
 @app.route('/test', methods=['POST'])
-def test():
-    return "Hello"
+def string_to_cut():
+    srting_to_cut = request.json['string_to_cut']
 
 
 def string_cutter(string):
-    return_value = [string[i] for i in range(len(string)) if (i+1)%3==0]
-    return ''.join(return_value)
+    cut_string = [string[i] for i in range(len(string)) if (i+1)%3==0]
+    return ''.join(cut_string)
 
 if __name__=="__main__":
     app.run()
