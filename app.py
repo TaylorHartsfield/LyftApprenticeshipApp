504ce35ed8a2f184d73690485100c8d8e25a7b45
Taylor Hartsfield <taylormhartsfield@gmail.com>
2022-05-09 20:26:31 -0600

    String Cutter Function

--- app.py
+++ app.py
@@ -5,3 +5,11 @@ app = Flask(__name__)
 @app.route('/test', methods=['POST'])
 def test():
     return "Hello"
+
+
+def string_cutter(string):
+    return_value = [string[i] for i in range(len(string)) if (i+1)%3==0]
+    return ''.join(return_value)
+
+if __name__=="__main__":
+    app.run()
