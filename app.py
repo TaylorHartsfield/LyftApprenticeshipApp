9018ecff842df228ae6f2b0d21b11a3189aa56d1
Taylor Hartsfield <taylormhartsfield@gmail.com>
2022-05-09 21:50:47 -0600

    Docstrings

--- app.py
+++ app.py
@@ -4,17 +4,25 @@ app = Flask(__name__)
 
 @app.route('/test', methods=['POST'])
 def string_to_cut():
+    '''
+    A POST request route that recieves a JSON object containing a string value
+    and returns the parsed string by calling the string_cutter function, or an error
+    if the entry was not a valid JSON object'''
     string = request.json
     if string:
         cut_string = string_cutter(string['string_to_cut'])
         return jsonify({"return_string": cut_string})
     return jsonify({"error": "Invalid Entry"})
 
+
 def string_cutter(string):
-    if len(string)<3:
+    '''function to parse over the string parameter
+    and return a string with every 3rd character'''
+    if len(string)<3: #edge case to handle input less than 3 characters
         return "String value too short. Must enter a string of at least 3 characters."
-    cut_string = [string[i] for i in range(len(string)) if (i+1)%3==0]
+    cut_string = [string[i] for i in range(len(string)) if (i+1)%3==0] 
     return ''.join(cut_string)
+
 
 if __name__=="__main__":
     app.run()
