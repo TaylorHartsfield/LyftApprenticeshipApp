34cec4cae3c8694b19a4cba4d5580931930a0ea6
Taylor Hartsfield <taylormhartsfield@gmail.com>
2022-05-09 22:11:12 -0600

    Typo Fix

--- app.py
+++ app.py
@@ -8,9 +8,9 @@ def string_to_cut():
     A POST request route that recieves a JSON object containing a string value
     and returns the parsed string by calling the string_cutter function, or an error
     if the entry was not a valid JSON object'''
-    string = request.json
-    if string:
-        cut_string = string_cutter(string['string_to_cut'])
+    string_to_cut = request.json
+    if string_to_cut:
+        cut_string = string_cutter(string_to_cut['string_to_cut'])
         return jsonify({"return_string": cut_string})
     return jsonify({"error": "Invalid Entry"})
 
@@ -19,7 +19,7 @@ def string_cutter(string):
     '''function to parse over the string parameter
     and return a string with every 3rd character'''
     if len(string)<3: #edge case to handle input less than 3 characters
-        return "String value too short. Must enter a string of at least 3 characters."
+        return f'Cannot cut a string with less than 3 characters: {string}'
     cut_string = [string[i] for i in range(len(string)) if (i+1)%3==0] 
     return ''.join(cut_string)
 
